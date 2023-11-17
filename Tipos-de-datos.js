20c7fd9eca932538ff0f8e9f0214e7a40b762c8d
yeison02 <yeison.suarezt@gmail.com>
2023-11-17 11:33:38 -0500

    Tipos de datos y operadores aritmeticos

---
+++ Tipos-de-datos.js
@@ -0,0 +1,22 @@
+/*Tipos de datos
+
+Datos primitivos: Numeros, cadenas de texto, boleanos, null, undefined, bigint, symbol
+
+Operadores aritmeticos:
++ : suma
+- : resta
+* : multiplicacion
+/ : division
+% : modulo (resto de la division)
+** : Exponente
+
+Concatenacion: para unir dos cadenas de texto, podemos usar el operador +*/
+
+let numero = 2;
+console.log(numero);
+
+let cadena = "Hola mundo";
+console.log(cadena);
+
+let boleanos = false;
+console.log(boleanos);
