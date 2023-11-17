35c700b04321c11354cca1bdb428b46c1da5f990
yeison02 <yeison.suarezt@gmail.com>
2023-11-17 11:49:33 -0500

    Operadores de comparacion

---
+++ operadores-de-comparacion.js
@@ -0,0 +1,31 @@
+//Los operadores de comparación en JavaScript nos permiten comparar dos valores. Siempre devuelven un valor booleano (true o false).
+/*
+> : mayor
+< : menor
+5 > 3 // true
+5 < 3 // false
+
+>= : mayor o igual
+<= : menor o igual
+5 >= 3 // true
+5 >= 5 // true
+5 <= 3 // false
+5 <= 5 // true
+
+=== : iguales estrictas
+!== : desigualdades estrictas
+5 === 5 // true
+5 !== 5 // false
+
+Comparando cadenas de texto
+los operadores de comparacion tambien los podemos usar para comparar cadenas de texto y otros tipos de datos
+'JavaScript' === 'JavaScript' // true
+'JavaScript' === 'Java' // false
+"JavaScript" !== 'PHP' // true
+`Estoy Aprendiendo JavaScript` === 'Estoy Aprendiendo JavaScript' // true
+
+Comparando booleanos
+true === true // true
+true === false // false
+false !== false // false
+*/
