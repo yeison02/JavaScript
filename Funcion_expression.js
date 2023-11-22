154f63c5a1dc3ddf30eb1e71e27ccb627793fad8
yeison02 <yeison.suarezt@gmail.com>
2023-11-22 11:37:35 -0500

    Funcion espression y que es (hoisting)

---
+++ Funcion_expression.js
@@ -0,0 +1,16 @@
+/*
+Existen dos formas más de declarar funciones en JavaScript: function expression y arrow function. En este caso, hablemos de la primera.
+
+¿Qué es una function expression?
+Una function expression es una función que se asigna a una variable. Por ejemplo:
+*/
+
+//Esto es una funcion expression
+const sumar = function (a, b) {
+  return a + b;
+};
+
+//Esto es una declaracion de funcion
+function suma(a, b) {
+  return a + b;
+}
