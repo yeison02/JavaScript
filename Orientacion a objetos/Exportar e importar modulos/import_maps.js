e55486e3f9371d88ad19162bcd880796233e4914
yeison02 <yeison.suarezt@gmail.com>
2024-02-04 18:56:22 -0500

    Import maps, controla las rutas de donde se decargan los modulos

---
+++ Orientacion a objetos/Exportar e importar modulos/import_maps.js
@@ -0,0 +1,7 @@
+import { getData } from "./utils.js"; // Importacion relativa
+import utils from "./modules/utils.js"; // Importacion por defecto
+import { Howl } from "https://unpkg.com/mitt@3.0.0?module"; //Importacion desde URL
+
+//Bare import
+import { Howl } from "howler";
+import dayjs from "dayjs";
